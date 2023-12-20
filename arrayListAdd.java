ff5967777170fbd00ba8787033692a9a601eb905
Rajan Singh <ssrajan841@gmail.com>
2023-12-20 11:00:41 +0530

    just chech my VS code settings

---
+++ arrayListAdd.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+class arrayListAdd {
+    public static void main(String args[]) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        // add elements
+        list.add(2);
+        list.add(3);
+        list.add(5);
+
+        System.out.println(list);
+    }
+}
